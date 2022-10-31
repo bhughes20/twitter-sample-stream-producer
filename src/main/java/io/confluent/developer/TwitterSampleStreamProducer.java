3c6eb120819fe898eba39041dd43df128b400e5c
bhughes20 <bhughes567@gmail.com>
2022-10-31 19:22:00 +0000

    TSSP-003 get API stream response entity

--- src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
+++ src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
@@ -2,6 +2,7 @@ package io.confluent.developer;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
@@ -29,13 +30,19 @@ public class TwitterSampleStreamProducer {
         return response;
     }
 
+    private static HttpEntity getResponseEntity(HttpResponse response) {
+        HttpEntity entity = response.getEntity();
+        return entity;
+    }
+
     public static void main(String[] args) throws IOException, URISyntaxException {
         Dotenv dotenv = Dotenv.load();
         String bearerToken = dotenv.get("BEARER_TOKEN");
         String uri = "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at&expansions=author_id";
 
         if (!ObjectUtils.isEmpty(bearerToken)) {
-            getTwitterSampleStreamResponse(bearerToken, uri);
+            HttpResponse response = getTwitterSampleStreamResponse(bearerToken, uri);
+            getResponseEntity(response);
         } else {
             System.out.println("Please provide a valid bearer token to access the Twitter API");
         }
