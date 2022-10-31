8e4515e1b5c358aa3a6d462d5d96546e75e4a7c4
bhughes20 <bhughes567@gmail.com>
2022-10-31 21:56:00 +0000

    TSSP-004 implementing producer without a schema registry

--- src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
+++ src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
@@ -1,5 +1,11 @@
 package io.confluent.developer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.Producer;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -16,9 +22,13 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class TwitterSampleStreamProducer {
 
@@ -46,18 +56,50 @@ public class TwitterSampleStreamProducer {
         return entity;
     }
 
-    private static void produceToTopic (HttpEntity entity) throws IOException {
+    private static void produceToTopic (HttpEntity entity, Producer<String, String> producer, String topic) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader((entity.getContent())));
         String line = reader.readLine();
         ObjectMapper objectMapper = new ObjectMapper();
+
         while (line != null) {
             JsonNode jsonNode = objectMapper.readTree(line);
-            System.out.println(jsonNode.get("data"));
+            String key = jsonNode.get("data").get("id").toString();
+            String value = jsonNode.get("data").toString();
+            producer.send(new ProducerRecord<String, String>(topic, key, value), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata m, Exception e) {
+                    if (e != null) {
+                        e.printStackTrace();
+                    } else {
+                        System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+                    }
+                }
+            });
             line = reader.readLine();
         }
     }
 
+    public static Properties loadProperties(String fileName) throws IOException {
+        if (!Files.exists(Paths.get(fileName))) {
+            throw new IOException(fileName + " not found.");
+        }
+        final Properties props = new Properties();
+        final FileInputStream input = new FileInputStream(fileName);
+        props.load(input);
+        input.close();
+        return props;
+    }
+
+
     public static void main(String[] args) throws IOException, URISyntaxException {
+        if(args.length != 1){
+            throw new IllegalArgumentException("Please provide the configuration/properties file path as a command line argument");
+        }
+
+        final Properties props = TwitterSampleStreamProducer.loadProperties(args[0]);
+        final String topic = props.getProperty("output.topic.name");
+        final Producer<String, String> producer = new KafkaProducer<>(props);
+
         Dotenv dotenv = Dotenv.load();
         String bearerToken = dotenv.get("BEARER_TOKEN");
         String uri = "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at&expansions=author_id";
@@ -66,7 +108,7 @@ public class TwitterSampleStreamProducer {
             HttpResponse response = getTwitterSampleStreamResponse(bearerToken, uri);
             HttpEntity responseEntity = getResponseEntity(response);
             if (!ObjectUtils.isEmpty(responseEntity)) {
-                produceToTopic(responseEntity);
+                produceToTopic(responseEntity, producer, topic);
             }
         } else {
             System.out.println("Please provide a valid bearer token to access the Twitter API");
