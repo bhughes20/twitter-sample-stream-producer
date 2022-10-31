3a0d4721e1d157293e86e00f84f194edbd80e1b0
bhughes20 <bhughes567@gmail.com>
2022-10-31 19:42:16 +0000

    TSSP-003 read twitter API stream line by line and get json data

--- src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
+++ src/main/java/io/confluent/developer/TwitterSampleStreamProducer.java
@@ -1,17 +1,23 @@
 package io.confluent.developer;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
 public class TwitterSampleStreamProducer {
@@ -27,12 +33,28 @@ public class TwitterSampleStreamProducer {
         HttpGet httpGet = new HttpGet(uriBuilder.build());
         httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken));
         HttpResponse response = httpClient.execute(httpGet);
+
+        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK ) {
+            throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+        }
+
         return response;
     }
 
     private static HttpEntity getResponseEntity(HttpResponse response) {
         HttpEntity entity = response.getEntity();
         return entity;
+    }
+
+    private static void produceToTopic (HttpEntity entity) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader((entity.getContent())));
+        String line = reader.readLine();
+        ObjectMapper objectMapper = new ObjectMapper();
+        while (line != null) {
+            JsonNode jsonNode = objectMapper.readTree(line);
+            System.out.println(jsonNode.get("data"));
+            line = reader.readLine();
+        }
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
@@ -42,7 +64,10 @@ public class TwitterSampleStreamProducer {
 
         if (!ObjectUtils.isEmpty(bearerToken)) {
             HttpResponse response = getTwitterSampleStreamResponse(bearerToken, uri);
-            getResponseEntity(response);
+            HttpEntity responseEntity = getResponseEntity(response);
+            if (!ObjectUtils.isEmpty(responseEntity)) {
+                produceToTopic(responseEntity);
+            }
         } else {
             System.out.println("Please provide a valid bearer token to access the Twitter API");
         }
